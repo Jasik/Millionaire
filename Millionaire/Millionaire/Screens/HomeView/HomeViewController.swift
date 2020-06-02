e503547df8fc28b193310a4690147dbdff06264b
Jasik <rogozhkin87@gmail.com>
2020-06-02 22:02:29 +0900

    Update setting for project

---
+++ Millionaire/Millionaire/Screens/HomeView/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  Millionaire
+//
+//  Created by Vladimir Rogozhkin on 2020/06/02.
+//  Copyright © 2020 Vladimir Rogozhkin. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ Millionaire/Millionaire/SharedObjects/MainNavigationController.swift
@@ -0,0 +1,18 @@
+//
+//  MainNavigationController.swift
+//  Millionaire
+//
+//  Created by Vladimir Rogozhkin on 2020/06/02.
+//  Copyright © 2020 Vladimir Rogozhkin. All rights reserved.
+//
+
+import UIKit
+
+class MainNavigationController: UINavigationController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationBar.prefersLargeTitles = true
+    }
+}
