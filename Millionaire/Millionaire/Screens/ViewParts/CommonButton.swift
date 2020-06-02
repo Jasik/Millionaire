db2648819e14718c2be331c8159597d858476610
Jasik <rogozhkin87@gmail.com>
2020-06-02 23:33:09 +0900

    added class for UIButton

--- Millionaire/Millionaire/Screens/ViewParts/CommonButton.swift
+++ Millionaire/Millionaire/Screens/ViewParts/CommonButton.swift
@@ -8,4 +8,29 @@
 
 import UIKit
 
-
+final class CommonButton: UIButton {
+    
+    init(text: String, width: CGFloat = 90.0, height: CGFloat = 30.0, color: UIColor, radius: CGFloat = 8.0) {
+        super.init(frame: .zero)
+        
+        setupContstraints(width, height)
+        setupStyles(text, color, radius)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    private func setupContstraints(_ width: CGFloat, _ height: CGFloat) {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.widthAnchor.constraint(equalToConstant: width).isActive = true
+        self.heightAnchor.constraint(equalToConstant: height).isActive = true
+    }
+    
+    private func setupStyles(_ text: String, _ color: UIColor, _ radius: CGFloat) {
+        backgroundColor = color
+        setTitle(text, for: .normal)
+        layer.cornerRadius = radius
+    }
+    
+}
