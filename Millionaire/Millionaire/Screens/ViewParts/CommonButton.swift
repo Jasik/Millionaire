24060ff5ad20863174da5b7ed616c8da0303ae82
Jasik <rogozhkin87@gmail.com>
2020-06-02 22:14:13 +0900

    created file for common Button

---
+++ Millionaire/Millionaire/Screens/ViewParts/CommonButton.swift
@@ -0,0 +1,11 @@
+//
+//  CommonButton.swift
+//  Millionaire
+//
+//  Created by Vladimir Rogozhkin on 2020/06/02.
+//  Copyright © 2020 Vladimir Rogozhkin. All rights reserved.
+//
+
+import UIKit
+
+
