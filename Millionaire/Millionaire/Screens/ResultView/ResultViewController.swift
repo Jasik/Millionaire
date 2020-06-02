328d177c5d93fb75c2f8c27c87fe377ddf97e615
Jasik <rogozhkin87@gmail.com>
2020-06-02 23:29:28 +0900

    Created GameViewController, ResultViewController

---
+++ Millionaire/Millionaire/Screens/ResultView/ResultViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ResultViewController.swift
+//  Millionaire
+//
+//  Created by Vladimir Rogozhkin on 2020/06/02.
+//  Copyright © 2020 Vladimir Rogozhkin. All rights reserved.
+//
+
+import Foundation
