99a58877025dd1fe98e99e09cc91b6330f20d08d
Jasik <rogozhkin87@gmail.com>
2020-06-02 21:43:19 +0900

    Created project

---
+++ Millionaire/Millionaire/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Millionaire
+//
+//  Created by Vladimir Rogozhkin on 2020/06/02.
+//  Copyright © 2020 Vladimir Rogozhkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Millionaire/MillionaireTests/MillionaireTests.swift
@@ -0,0 +1,34 @@
+//
+//  MillionaireTests.swift
+//  MillionaireTests
+//
+//  Created by Vladimir Rogozhkin on 2020/06/02.
+//  Copyright © 2020 Vladimir Rogozhkin. All rights reserved.
+//
+
+import XCTest
+@testable import Millionaire
+
+class MillionaireTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
