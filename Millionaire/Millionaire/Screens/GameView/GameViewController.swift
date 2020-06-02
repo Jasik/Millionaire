bfa7bb4088cd06601c326ac7c36af1e051b56cf2
Jasik <rogozhkin87@gmail.com>
2020-06-02 23:32:29 +0900

    added title for Result and Game ViewController

--- Millionaire/Millionaire/Screens/GameView/GameViewController.swift
+++ Millionaire/Millionaire/Screens/GameView/GameViewController.swift
@@ -6,4 +6,15 @@
 //  Copyright © 2020 Vladimir Rogozhkin. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+        navigationItem.title = "Game"
+    }
+    
+}
--- Millionaire/Millionaire/Screens/ResultView/ResultViewController.swift
+++ Millionaire/Millionaire/Screens/ResultView/ResultViewController.swift
@@ -6,4 +6,14 @@
 //  Copyright © 2020 Vladimir Rogozhkin. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+        navigationItem.title = "Results"
+    }
+}
