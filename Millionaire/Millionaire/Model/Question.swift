28a2f1f4b5c687ca58d0fabf8336f90cd7c8c05b
Jasik <rogozhkin87@gmail.com>
2020-06-04 00:00:06 +0900

    created Question

--- Millionaire/Millionaire/Model/Question.swift
+++ Millionaire/Millionaire/Model/Question.swift
@@ -7,3 +7,16 @@
 //
 
 import Foundation
+
+struct Question {
+    
+    let question: String
+    let answerOptions: [String]
+    let answer: String
+    
+    init(question: String, answerOptions: [String], answer: String) {
+        self.question = question
+        self.answerOptions = answerOptions
+        self.answer = answer
+    }
+}
