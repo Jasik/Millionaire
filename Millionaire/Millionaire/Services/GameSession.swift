8715771601e644d2d07e086b48bf2f9370f289f8
Jasik <rogozhkin87@gmail.com>
2020-06-04 20:01:23 +0900

    created Game and GameSession files

---
+++ Millionaire/Millionaire/Services/GameSession.swift
@@ -0,0 +1,9 @@
+//
+//  GameSession.swift
+//  Millionaire
+//
+//  Created by Vladimir Rogozhkin on 2020/06/04.
+//  Copyright © 2020 Vladimir Rogozhkin. All rights reserved.
+//
+
+import Foundation
