9506732d9a95e7db5440a0fda7c0f44f79ec5e76
Jasik <rogozhkin87@gmail.com>
2020-06-06 21:39:19 +0900

    updated Caretaker

--- Millionaire/Millionaire/Services/RecordsCaretaker.swift
+++ Millionaire/Millionaire/Services/RecordsCaretaker.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+
+final class RecordCaretaker {
+    
+    private let encoder = JSONEncoder()
+    private let decoder = JSONDecoder()
+    
+    private let key = "records"
+    
+    func save(records: [Result]) {
+        do {
+            let data = try self.encoder.encode(records)
+            UserDefaults.standard.set(data, forKey: key)
+        } catch {
+            print(error)
+        }
+    }
+    
+    func retrieveRecords() -> [Result] {
+        
+        guard let data = UserDefaults.standard.data(forKey: key) else {
+            return []
+        }
+        
+        do {
+            return try self.decoder.decode([Result].self, from: data)
+        } catch {
+            print(error)
+            return []
+        }
+    }
+}
