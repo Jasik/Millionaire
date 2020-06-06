33e3e26c427d89f5b9ecb40b5a17b7b1a8b93076
Jasik <rogozhkin87@gmail.com>
2020-06-06 21:46:59 +0900

    added Date extension

--- Millionaire/Millionaire/Extensions/Foundation/Date+Extension.swift
+++ Millionaire/Millionaire/Extensions/Foundation/Date+Extension.swift
@@ -7,3 +7,11 @@
 //
 
 import Foundation
+
+extension Date {
+    func getFormattedDate() -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        return formatter.string(from: self)
+    }
+}
