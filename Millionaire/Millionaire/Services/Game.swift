2b8a9bfa82f1177449842d1129cd66a95721ad8d
Jasik <rogozhkin87@gmail.com>
2020-06-06 21:41:30 +0900

    updated Game

--- Millionaire/Millionaire/Services/Game.swift
+++ Millionaire/Millionaire/Services/Game.swift
@@ -13,6 +13,26 @@ final class Game {
     static let shared = Game()
     
     private var gameSession: GameSession?
+    private let recordsCaretaker = RecordCaretaker()
     
-    private init() {}
+    private(set) var results: [Result] {
+        didSet {
+            recordsCaretaker.save(records: self.results)
+        }
+    }
+    
+    func start(_ gameSession: GameSession) {
+        self.gameSession = gameSession
+    }
+    
+    func finish() {
+        guard let gameSession = self.gameSession else { return }
+        let result = Result(date: Date(), prize: gameSession.money)
+        results.append(result)
+        self.gameSession = nil
+    }
+    
+    private init() {
+        self.results = self.recordsCaretaker.retrieveRecords()
+    }
 }
